afee10632988f1c38166eb522828193aece93dd4
nessime7 <sara.przebinda@gmail.com>
2022-03-02 22:49:53 +0100

    1

---
+++ src/calculatorzad/Calculator9.java
@@ -0,0 +1,13 @@
+/*
+Kalkulator ma wszystkie funkcje
+Dodatkowo wszystkie działania mają być zapisywane w jakiejś strukturze danych,
+W taki sposób żeby mieć dodatkową opcje 4. Wypisz wszystkie wykonane działania
+np. 2+2=4
+3+3=6
+4+4=8
+ */
+
+package calculatorzad;
+
+public class Calculator9 {
+}
